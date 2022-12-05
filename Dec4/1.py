6f795db3c1c5f80db9dad5ebba86e711941feb18
Abd Kayali <akayali532@gmail.com>
2022-12-05 03:52:59 +0200

    finsing challenge1 for Dec4

--- Dec4/1.py
+++ Dec4/1.py
@@ -23,18 +23,32 @@ print (__file__)
 def init():
     print("init")
 
+    # to count how many sets fully overlap
+    count = 0
+
     # reading the file
-    f = open(os.path.join(dirname,"2.txt"), "r")
+    f = open(os.path.join(dirname,"1.txt"), "r")
     lines = f.readlines()
 
     # looping through all lines
     for line in lines:
 
+        line = line.rstrip()
         print(line)
+
+        # seperating and cleaing the sets from the line
+        temp = line.split(",")
+        set1 = temp[0].split("-")
+        set2 = temp[1].split("-")
+
+        if (int(set1[0]) <= int(set2[0]) and int(set1[1]) >= int(set2[1])) or (int(set1[0]) >= int(set2[0]) and int(set1[1]) <= int(set2[1])):
+            print("match")
+            count += 1
+        print("")
 
     f.close()
 
-
+    print("we have: " + str(count) + " fully overlaping cases") 
 
 
 init()
