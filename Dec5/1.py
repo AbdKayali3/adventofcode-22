694250843fac886075c61f5e1fce693f83f8b169
Abd Kayali <akayali532@gmail.com>
2022-12-05 16:43:44 +0200

    starting challenge1 for Dec5

---
+++ Dec5/1.py
@@ -0,0 +1,35 @@
+# Objectives
+# 
+
+# Tasks
+# 
+
+import os
+import glob
+from textwrap import wrap
+import time
+
+dirname = os.path.dirname(__file__)
+print (os.getcwd())
+print (dirname)
+print (os.path.join(os.getcwd(), 'test'))
+print (__file__)
+
+def init():
+    print("init")
+
+    # reading the file
+    f = open(os.path.join(dirname,"2.txt"), "r")
+    lines = f.readlines()
+
+    # looping through all lines
+    for line in lines:
+
+        print(line)
+
+    f.close()
+
+
+
+
+init()
